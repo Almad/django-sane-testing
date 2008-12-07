229e9e1d25a96346c02c96d3f64bb17a495dc8f0
almad <none@none>
2008-12-07 19:54:06 -0100

    Intial package layout + selenium RC driver

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
+import os
+import sys
+
+project_dir = 'djangosanetesting'
+
+# Dynamically calculate the version based on django.VERSION.
+version = __import__('djangosanetesting').__versionstr__
+setup(
+    name = "djangohttpdigest",
+    version = version,
+    url = 'http://devel.almad.net/trac/django-sane-testing/',
+    author = 'Lukas Linhart',
+    author_email = 'bugs@almad.net',
+    description = 'Support for various testing tools for Django web framework',
+    packages = ['djangohttpdigest'],
+    scripts = [],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.5",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
