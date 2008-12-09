c18f92049f44a9d651d0763c53f83eae8996e8ad
almad <none@none>
2008-12-09 00:36:27 -0100

    Scetch of nose test runner

---
+++ djangosanetesting/testrunner.py
@@ -0,0 +1,18 @@
+from django.test.utils import setup_test_environment, teardown_test_environment, create_test_db, destroy_test_db
+import nose
+
+def run_tests(test_labels, verbosity=1, interactive=True, extra_tests=[]):
+    """ Run tests with nose instead of defualt test runner """
+    setup_test_environment()
+    old_name = settings.DATABASE_NAME
+    create_test_db(verbosity, autoclobber=not interactive)
+    argv_backup = sys.argv
+    # we have to strip script name before passing to nose
+    sys.argv = argv_backup[0:1]
+    config = Config(files=all_config_files(), plugins=DefaultPluginManager())
+    nose.run(config=config)
+    sys.argv = argv_backup
+    destroy_test_db(old_name, verbosity)
+    teardown_test_environment()
+    run_tests.__test__ = False
+
