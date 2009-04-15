b04a36a180cb14a95980ad7e1159d45042511ef9
almad <none@none>
2009-04-15 05:14:42 -0200

    Version bump to 0.5.2

--- djangosanetesting/__init__.py
+++ djangosanetesting/__init__.py
@@ -1,6 +1,6 @@
 
-__version__ = (0, 5, 1, '')
-__versionstr__ = '0.5.1'
+__version__ = (0, 5, 2, '')
+__versionstr__ = '0.5.2'
 
 from djangosanetesting.cases import *
 from djangosanetesting.testrunner import *
