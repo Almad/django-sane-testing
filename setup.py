48edef912620e3dd209ca91cb7f6b93a3dbedcac
Almad <bugs@almad.net>
2010-10-02 15:07:59 +0200

    Provide better description for pypi

--- setup.py
+++ setup.py
@@ -18,7 +18,15 @@ setup(
     url = 'http://devel.almad.net/trac/django-sane-testing/',
     author = 'Lukas Linhart',
     author_email = 'bugs@almad.net',
-    description = 'Support sane testing in django using nose.',
+    description = u'''
+    Django: Sane testing
+    ======================
+
+    django-sane-testing integrates Django with Nose testing framework. Goal is to provide nose goodies to Django testing and to support feasible integration or functional testing of Django applications, for example by providing more control over transaction/database handling.
+    Thus, there is a way to start HTTP server for non-WSGI testing - like using Selenium or Windmill.
+    Selenium has also been made super easy - just start --with-selenium, inherit from SeleniumTestCase and use self.selenium.
+    Package is documented - see docs/ or http://getthedocs.org/Almad/djangosanetesting.
+    '''
     packages = ['djangosanetesting', 'djangosanetesting.selenium'],
     scripts = [],
     requires = ['Django (>=1.0)', 'nose (>=0.10)'],
