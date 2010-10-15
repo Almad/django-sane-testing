20f0c67cf1889b05161a7412ce3a28708b14a174
Lukas Linhart <lukas.linhart@centrumholdings.com>
2010-10-15 11:34:48 +0200

    Version bump

--- djangosanetesting/__init__.py
+++ djangosanetesting/__init__.py
@@ -1,6 +1,6 @@
 
-__version__ = (0, 5, 6, '')
-__versionstr__ = '0.5.6'
+__version__ = (0, 5, 7, '')
+__versionstr__ = '0.5.7'
 
 from djangosanetesting.cases import *
 from djangosanetesting.testrunner import *
