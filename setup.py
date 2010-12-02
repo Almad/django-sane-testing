a61ce741106cf60499374c05fb8383ee5e4aab5f
Lukas Linhart <lukas.linhart@centrumholdings.com>
2010-12-02 15:50:19 +0100

    Use paver as primary entry point

--- setup.py
+++ setup.py
@@ -1,53 +1,7 @@
-#!/usr/bin/env python
+import os
+if os.path.exists("paver-minilib.zip"):
+    import sys
+    sys.path.insert(0, "paver-minilib.zip")
 
-try:
-    import ez_setup
-    ez_setup.use_setuptools()
-except ImportError:
-    pass
-
-from setuptools import setup
-
-project_dir = 'djangosanetesting'
-name = 'djangosanetesting'
-
-version = '0.5.7'
-setup(
-    name = name,
-    version = version,
-    url = 'http://devel.almad.net/trac/django-sane-testing/',
-    author = 'Lukas Linhart',
-    author_email = 'bugs@almad.net',
-    description = u'''
-    Django: Sane testing
-    ======================
-
-    django-sane-testing integrates Django with Nose testing framework. Goal is to provide nose goodies to Django testing and to support feasible integration or functional testing of Django applications, for example by providing more control over transaction/database handling.
-    Thus, there is a way to start HTTP server for non-WSGI testing - like using Selenium or Windmill.
-    Selenium has also been made super easy - just start --with-selenium, inherit from SeleniumTestCase and use self.selenium.
-    Package is documented - see docs/ or http://getthedocs.org/Almad/djangosanetesting.
-    ''',
-    packages = ['djangosanetesting', 'djangosanetesting.selenium'],
-    scripts = [],
-    requires = ['Django (>=1.1)', 'nose (>=0.10)'],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.5",
-        "Programming Language :: Python :: 2.6",
-        "Topic :: Software Development :: Testing",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    entry_points = {
-        'nose.plugins.0.10': [
-            'djangoliveserver = %s.noseplugins:DjangoLiveServerPlugin' % name,
-            'cherrypyliveserver = %s.noseplugins:CherryPyLiveServerPlugin' % name,
-            'django = %s.noseplugins:DjangoPlugin' % name,
-            'selenium = %s.noseplugins:SeleniumPlugin' % name,
-            'sanetestselection = %s.noseplugins:SaneTestSelectionPlugin' % name,
-            'djangotranslations = %s.noseplugins:DjangoTranslationPlugin' % name, 
-        ]
-    }
-)
+import paver.tasks
+paver.tasks.main()
